75e678ec1b1b023275f8598d3c4ebe397b326100
Leta <l.jifar.sssup.it@gmail.com>
2014-11-16 21:38:44 +0100

    sound play setting

--- src/org/jLeta/myfirstandroidapp/Splash.java
+++ src/org/jLeta/myfirstandroidapp/Splash.java
@@ -2,19 +2,22 @@ package org.jLeta.myfirstandroidapp;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 
 /**
  * Splash screen
- * 
+ *
  * @author jLeta
- * 
+ *
  */
 public class Splash extends Activity {
 
 	// Song is removed to testing installing the application with few
 	// memoryspace.
-	// MediaPlayer ourSong;
+	MediaPlayer ourSong;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +25,16 @@ public class Splash extends Activity {
 		// display the splash activity
 		setContentView(R.layout.splash);
 
-		// Play music at background
-		// ourSong = MediaPlayer.create(Splash.this, R.raw.godisnotgead);
-		// ourSong.start();
+		//get preference settings
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+		//check if music play is activated, consider that value as true if it is not defined yet.
+		boolean music = prefs.getBoolean("checkbox", true);
+		if(music){
+			// Play music at background
+			ourSong = MediaPlayer.create(Splash.this, R.raw.godisnotgead);
+			ourSong.start();
+		}
 
 		// Create thread to call the main activity after some sleep time
 		Thread timer = new Thread() {
@@ -44,13 +54,13 @@ public class Splash extends Activity {
 	}
 
 	/*
-	 //Un comment this this to pause music play 
+	 //Un comment this this to pause music play
 	@Override
 	protected void onPause() {
 		super.onPause();
 		ourSong.stop();
 		finish();
 	}
-	
+
 	*/
 }
